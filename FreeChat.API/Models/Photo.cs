dab4d3a91135f85da99b48772a534a764cf46b47
Vasanth1009 <vasanthprabhakaran1999@gmail.com>
2020-09-12 16:59:28 +0530

    Pre-publishing

--- FreeChat.API/Models/Photo.cs
+++ FreeChat.API/Models/Photo.cs
@@ -8,7 +8,7 @@ namespace FreeChat.API.Models {
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
-        public User User { get; set; }
+        public virtual User User { get; set; }
         public int UserId { get; set; }
     }
 }
--- FreeChat.API/Startup.cs
+++ FreeChat.API/Startup.cs
@@ -22,7 +22,26 @@ namespace FreeChat.API {
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext> (x => {
+                x.UseLazyLoadingProxies();
+                x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
+            });
+
+            ConfigureServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext> (x => {
+                x.UseLazyLoadingProxies();
+                x.UseMySql (Configuration.GetConnectionString ("DefaultConnection"));
+            });
+
+            ConfigureServices(services);
+        }
+        
         public void ConfigureServices (IServiceCollection services) {
             services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
             services.AddControllers ().AddNewtonsoftJson (opt => {
@@ -42,7 +61,7 @@ namespace FreeChat.API {
                 ValidateAudience = false
                 };
             });
-            services.AddScoped<LogUserActivity>();
+            services.AddScoped<LogUserActivity> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,14 +86,18 @@ namespace FreeChat.API {
 
             app.UseRouting ();
 
-            app.UseCors (x => x.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
 
             app.UseAuthentication ();
-
             app.UseAuthorization ();
+
+            app.UseCors (x => x.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
+            
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
 
             app.UseEndpoints (endpoints => {
                 endpoints.MapControllers ();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
