fe03520e2f4b41f7c79b836b8774d1ce5cea8088
Vasanth1009 <vasanthprabhakaran1999@gmail.com>
2020-09-14 21:13:33 +0530

    Identity Starting Point

--- FreeChat.API/Startup.cs
+++ FreeChat.API/Startup.cs
@@ -14,9 +14,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 
-namespace FreeChat.API {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace FreeChat.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
@@ -24,9 +27,10 @@ namespace FreeChat.API {
 
         public void ConfigureDevelopmentServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext> (x => {
+            services.AddDbContext<DataContext>(x =>
+            {
                 x.UseLazyLoadingProxies();
-                x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
+                x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
             ConfigureServices(services);
@@ -34,41 +38,50 @@ namespace FreeChat.API {
 
         public void ConfigureProductionServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext> (x => {
+            services.AddDbContext<DataContext>(x =>
+            {
                 x.UseLazyLoadingProxies();
-                x.UseMySql (Configuration.GetConnectionString ("DefaultConnection"));
+                x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
             ConfigureServices(services);
         }
-        
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
-            services.AddControllers ().AddNewtonsoftJson (opt => {
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(opt =>
+            {
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
-            services.AddCors ();
-            services.Configure<CloudinarySettings> (Configuration.GetSection ("CloudinarySettings"));
-            services.AddAutoMapper (typeof (ChattingRepository).Assembly);
+            services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.AddAutoMapper(typeof(ChattingRepository).Assembly);
             // services.AddTransient<Seed>();
-            services.AddScoped<IAuthRepository, AuthRepository> ();
-            services.AddScoped<IChattingRepository, ChattingRepository> ();
-            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme).AddJwtBearer (options => {
-                options.TokenValidationParameters = new TokenValidationParameters {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey (Encoding.ASCII.GetBytes (Configuration.GetSection ("AppSettings:Token").Value)),
-                ValidateIssuer = false,
-                ValidateAudience = false
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IChattingRepository, ChattingRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
                 };
             });
-            services.AddScoped<LogUserActivity> ();
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
-            } else {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
                 app.UseExceptionHandler (builder => {
                     builder.Run (async context => {
                         context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
@@ -80,23 +93,24 @@ namespace FreeChat.API {
                         }
                     });
                 });
+                app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-            app.UseRouting ();
+            app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-            app.UseAuthentication ();
-            app.UseAuthorization ();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-            app.UseCors (x => x.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
-            
-            app.UseDefaultFiles ();
-            app.UseStaticFiles ();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllers ();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
                 endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
